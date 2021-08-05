9a78f86c75e2dd28ed0aa3b41f7328d574323429
PRAKYATH L V <74598370+prakyathlv@users.noreply.github.com>
2021-08-05 10:09:48 +0530

    modifiers included

---
+++ data_type_modifiers_2.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;//declaration
+    a=12;//initialization
+    cout<<"size of int "<<sizeof(a)<<endl;
+
+    float b;
+    cout<<"size of float "<<sizeof(b)<<endl;
+
+    char c;
+    cout<<"size of char "<<sizeof(c)<<endl;
+
+    bool d;
+     cout<<"size of bool "<<sizeof(d)<<endl;
+   
+    short i;
+     cout<<"size of short "<<sizeof(i)<<endl;
+
+     long j;
+      cout<<"size of long "<<sizeof(j)<<endl;
+
+
+
+
+
+}
