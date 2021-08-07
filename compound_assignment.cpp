8a1cac9a4bfeeedac86a1462d808b1767d6a41ec
PRAKYATH L V <74598370+prakyathlv@users.noreply.github.com>
2021-08-07 09:13:37 +0530

    compound assignment

---
+++ compound_assignment.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int sum=5,a=10,b=20,c=30;
+	sum=sum+a+b+c;
+	cout<<sum<<endl;
+	sum=5;
+	sum=sum+a;
+	sum=sum+b;
+	sum=sum+c;
+	cout<<sum<<endl;
+	
+}
