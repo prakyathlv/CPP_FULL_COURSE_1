39a89aead778dce75350ddee53a2207cda90158a
PRAKYATH L V <74598370+prakyathlv@users.noreply.github.com>
2021-08-07 07:56:35 +0530

    perimeter of the rectangle

---
+++ perimeter_of_rectangle.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int l,b,p;
+	cout<<"enter the length and breadth of rectangle\n";
+	cin>>l >>b;
+	p=(l+b)/2;
+	cout<<"the perimeter of the rectangle is "<<p;
+}
