263d27b991a12d16174729927f884bb7658306e7
PRAKYATH L V <74598370+prakyathlv@users.noreply.github.com>
2021-08-07 07:50:35 +0530

    area of a triangle

---
+++ area_of_triangle.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float a,b,l;
+    cout<<"enter length and breadth\n";
+    cin>>l >>b ;
+    a=(b*l)/2;
+    cout<<"the area of triangle is "<< a;
+
+} 
