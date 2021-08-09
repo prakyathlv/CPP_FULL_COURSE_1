50498715e17f8172a42c869d8f8c98cf4170fdea
PRAKYATH L V <74598370+prakyathlv@users.noreply.github.com>
2021-08-09 12:31:16 +0530

    greatest of two numbers

---
+++ max_of_2_no..cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float a,b;
+	cout<<"enter the two numbers "<<endl;
+	cin>>a>>b;
+	if(a>b)
+	cout<<"greater number is: "<<a<<endl;
+	else
+	cout<<"greater number is: "<<b<<endl;
+	return 0;
+}
