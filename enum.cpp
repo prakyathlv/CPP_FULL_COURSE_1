8fc7faa7ff567b9da243728b6dbe20f2234c6b0e
PRAKYATH L V <74598370+prakyathlv@users.noreply.github.com>
2021-08-09 10:36:56 +0530

    enum example

---
+++ enum.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+enum branch {cs=50,ec,is=30,mech=70,others};
+
+int main()
+{
+	branch bit;
+	bit=ec;
+	cout<<bit<<endl;
+    cout<<cs<<endl;
+	cout<<ec<<endl;
+	cout<<is<<endl;
+	cout<<mech<<endl;
+	cout<<others<<endl;	
+}
