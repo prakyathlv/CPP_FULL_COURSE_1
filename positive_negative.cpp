3bab709e04e9b9f4555a312d237504204c7bb7a4
PRAKYATH L V <74598370+prakyathlv@users.noreply.github.com>
2021-08-09 12:41:35 +0530

    checking the number is positive or negative using if else condition

---
+++ positive_negative.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a;
+	cout<<"enter the number "<<endl;
+	cin>>a;
+	if(a>=0)
+	cout<<"the number "<<a<<" is positive."<<endl;
+	else
+	cout<<"the number "<<a<<" is negative."<<endl;
+	return 0;
+}
