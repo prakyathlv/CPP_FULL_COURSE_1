3e2b258ada642a78f4f80ff35ced95766359dff9
PRAKYATH L V <74598370+prakyathlv@users.noreply.github.com>
2021-08-09 12:42:27 +0530

    checking the number is evn or odd using if else condition

---
+++ even_odd.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a;
+	cout<<"enter the number "<<endl;
+	cin>>a;
+	if(a%2==0)
+	cout<<"the number "<<a<<" is even."<<endl;
+	else
+	cout<<"the number "<<a<<" is odd."<<endl;
+	return 0;
+}
