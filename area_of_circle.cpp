a15ebd43167553d551ba29de14827b8c060e9cd4
PRAKYATH L V <74598370+prakyathlv@users.noreply.github.com>
2021-08-09 10:51:40 +0530

    area of a circle

---
+++ area_of_circle.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+#define pie 3.14
+int main()
+{
+	float r,area;
+	cout<<"enter the radius of the circle"<<endl;
+	cin>>r;
+	area=pie*r*r;
+	cout<<area<<endl;
+	return 0;
+}
