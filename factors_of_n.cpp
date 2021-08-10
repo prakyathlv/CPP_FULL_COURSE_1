ba1ac52275da1fc084ff9fce7638c1f87d18958e
PRAKYATH L V <74598370+prakyathlv@users.noreply.github.com>
2021-08-10 09:21:01 +0530

    factors of a number

---
+++ factors_of_n.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cout<<"enter the number\n";
+	cin>>n;
+	cout<<"factors of "<<n<<" are ";
+	for(int i=1;i<=n/2;i++)
+	{
+		if(n%i==0)
+		{
+			cout<<i<<" ";
+		}
+	}
+	return 0;
+}
