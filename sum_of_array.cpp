840ae84e61be85107a6a1f85f4a503b63a4a2eb2
PRAKYATH L V <74598370+prakyathlv@users.noreply.github.com>
2021-08-11 09:59:58 +0530

    to find sum of array elements

---
+++ sum_of_array.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a[6],n,sum=0;
+	cout<<"enter the array elements\n";
+	for(int i=0;i<6;i++)
+	{
+	
+	cin>>a[i];
+}
+	for(int i=0;i<6;i++)
+	{
+		sum=sum+a[i];
+	}
+	cout<<"sum is "<<sum<<endl;
+	return 0;
+}
