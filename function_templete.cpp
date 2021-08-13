1d54f697b99cb8365d1b2fe5cfdf49396331f4fa
PRAKYATH L V <74598370+prakyathlv@users.noreply.github.com>
2021-08-13 10:43:02 +0530

    example for function template

---
+++ function_templete.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+template<class t>
+t maxim(t x,t y)
+{
+return x>y?x:y;
+}
+int main()
+{
+	cout<<maxim(12,14)<<endl;
+	cout<<maxim(2.3,1.4)<<endl;
+	cout<<maxim(2.3f,5.6f)<<endl;
+	return 0;
+}
+
+
